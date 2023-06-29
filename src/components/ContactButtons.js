2699903b845af74321cb3b933eeeb734bc7ce2ce
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-06-29 00:23:58 -0500

    ajustes

--- src/components/ContactButtons.js
+++ src/components/ContactButtons.js
@@ -3,6 +3,7 @@ import React from "react";
 import Email from "./Icons/Email";
 import Phone from "./Icons/Phone";
 import Location from "./Icons/Location";
+import WebsiteIcon from "./Icons/WebsiteIcon";
 
 const contactIcons = [
   {
@@ -20,6 +21,11 @@ const contactIcons = [
     icon: <Phone />,
     id: 3,
   },
+  {
+    iconType: "Website",
+    icon: <WebsiteIcon />,
+    id: 4,
+  },
 ];
 
 const ContactButtons = ({ contactData }) => {
@@ -29,7 +35,12 @@ const ContactButtons = ({ contactData }) => {
     <div className="flex gap-10 justify-center items-center w-full">
       {contactIcons.map(({ iconType, icon, id }) => {
         const dataItem = contactButtons.find((item) => item.titulo_de_boton === iconType);
-        const ref = dataItem?.titulo_de_boton === "Correo" ? `mailto:${dataItem.url}` : dataItem?.titulo_de_boton === "Llamar" ? `tel:${dataItem.url}` : dataItem?.url
+        const ref =
+          dataItem?.titulo_de_boton === "Correo"
+            ? `mailto:${dataItem.url}`
+            : dataItem?.titulo_de_boton === "Llamar"
+            ? `tel:${dataItem.url}`
+            : dataItem?.url;
         return dataItem ? (
           <div className="flex flex-col items-center gap-2">
             <a target="_blank" className=" h-16 w-16 bg-[#222222] p-4 rounded-full" href={ref}>
