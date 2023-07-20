8513e0bbbb82f30bc42ae48bf10368618e932354
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-07-19 22:45:12 -0500

    ajustes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -18,7 +18,7 @@ const Profile = ({ profileData }) => {
       <div className="-space-x-1 mt-[-45%] relative z-10 shadow-lg rounded-3xl">
         <div className="flex items-center bg-white rounded-3xl">
           <Image
-            className="rounded-l-3xl inline-block object-contain h-52 w-52 p-5"
+            className="rounded-l-3xl inline-block object-cover h-48 max-w-[50%]"
             src={Logo}
             width={500}
             height={500}
@@ -32,10 +32,10 @@ const Profile = ({ profileData }) => {
             height={500}
             alt={`Foto de ${nombre_apellido}`}
           /> */}
-          <div className="text-black w-full px-4 flex flex-col gap-y-3.5 items-center">
+          <div className="text-black w-full flex flex-col items-start gap-y-3.5 p-5">
             {/* <Image src={Logo} height={100} width={100} /> */}
-            <h1 className="font-bold text-4xl text-center">{nombre_apellido}</h1>
-            <p className="text-xs tracking-[3px] font-bold text-center">{description}</p>
+            <h1 className="font-bold text-3xl text-left">{nombre_apellido}</h1>
+            <p className="text-xs tracking-wider font-bold">{description}</p>
           </div>
         </div>
       </div>
